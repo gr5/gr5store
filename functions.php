9e7aecc4e527d60c0e1eee95cd62c15719375350
George Roberts <gr@gr5.org>
2021-10-08 15:44:50 -0400

    initial repo creation

---
+++ functions.php
@@ -0,0 +1,158 @@
+<?php
+// 
+// enqueue script for parent theme stylesheet
+//
+
+function gr5_parent_styles() {
+     
+    // enqueue style
+    wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );
+         
+}
+add_action( 'wp_enqueue_scripts', 'gr5_parent_styles');
+
+
+//
+// multi add to cart feature
+//
+
+function woocommerce_maybe_add_multiple_products_to_cart() {
+// Make sure WC is installed, and add-to-cart qauery arg exists, and contains at least one comma.
+if ( ! class_exists( 'WC_Form_Handler' ) || empty( $_REQUEST['add-to-cart'] ) || false === strpos( $_REQUEST['add-to-cart'], ',' ) ) {
+    return;
+}
+
+// Remove WooCommerce's hook, as it's useless (doesn't handle multiple products).
+remove_action( 'wp_loaded', array( 'WC_Form_Handler', 'add_to_cart_action' ), 20 );
+
+$product_ids = explode( ',', $_REQUEST['add-to-cart'] );
+$count       = count( $product_ids );
+$number      = 0;
+
+foreach ( $product_ids as $product_id ) {
+    if ( ++$number === $count ) {
+        // Ok, final item, let's send it back to woocommerce's add_to_cart_action method for handling.
+        $_REQUEST['add-to-cart'] = $product_id;
+
+        return WC_Form_Handler::add_to_cart_action();
+    }
+
+    $product_id        = apply_filters( 'woocommerce_add_to_cart_product_id', absint( $product_id ) );
+    $was_added_to_cart = false;
+    $adding_to_cart    = wc_get_product( $product_id );
+
+    if ( ! $adding_to_cart ) {
+        continue;
+    }
+
+    $add_to_cart_handler = apply_filters( 'woocommerce_add_to_cart_handler', $adding_to_cart->product_type, $adding_to_cart );
+
+    /*
+     * Sorry.. if you want non-simple products, you're on your own.
+     *
+     * Related: WooCommerce has set the following methods as private:
+     * WC_Form_Handler::add_to_cart_handler_variable(),
+     * WC_Form_Handler::add_to_cart_handler_grouped(),
+     * WC_Form_Handler::add_to_cart_handler_simple()
+     *
+     * Why you gotta be like that WooCommerce?
+     */
+    if ( 'simple' !== $add_to_cart_handler ) {
+        continue;
+    }
+
+    // For now, quantity applies to all products.. This could be changed easily enough, but I didn't need this feature.
+    $quantity          = empty( $_REQUEST['quantity'] ) ? 1 : wc_stock_amount( $_REQUEST['quantity'] );
+    $passed_validation = apply_filters( 'woocommerce_add_to_cart_validation', true, $product_id, $quantity );
+
+    if ( $passed_validation && false !== WC()->cart->add_to_cart( $product_id, $quantity ) ) {
+        wc_add_to_cart_message( array( $product_id => $quantity ), true );
+    }
+}
+}
+
+// Fire before the WC_Form_Handler::add_to_cart_action callback.
+add_action( 'wp_loaded',        'woocommerce_maybe_add_multiple_products_to_cart', 15 );
+
+
+
+
+
+//
+// CODE ONLY FOR HOME PAGE IS BELOW
+//
+
+function product_info_to_jscript($desc,$id) {
+  $_product = wc_get_product($id);
+  echo "gr_prices['$desc'] = ".$_product->get_price().";\n";
+  echo "gr_instock['$desc'] = ".$_product->is_in_stock().";\n";
+  echo "gr_pretty_price['$desc'] = '".$_product->get_price_html()."';\n";
+  if ($desc=='cube') echo "var gr_img_cube = '".$_product->get_image("woocommerce_gallery_thumbnail")."';\n";
+}
+
+function gr5_hook_javascript_footer() {
+  if (get_queried_object_id() != 21) // 21 is page id for home page
+     return;
+
+  // okay this is the "home" page
+  echo "<script>\n";
+  echo "var gr_prices=[];\n";
+  echo "var gr_instock=[];\n";
+  echo "var gr_pretty_price=[];\n";
+  product_info_to_jscript('laser17',44);
+  product_info_to_jscript('laser26',11);
+  product_info_to_jscript('lens765',39);
+  product_info_to_jscript('lens9',35);
+  product_info_to_jscript('lens13',38);
+  product_info_to_jscript('cube',33);
+  product_info_to_jscript('if',28);
+  product_info_to_jscript('flat',40);
+  product_info_to_jscript('xyz_asm',59);
+  product_info_to_jscript('xyz_kit',47);
+  product_info_to_jscript('xyz_plastic',65);
+  product_info_to_jscript('xyz_metal',62);
+  echo "var cart_items = ".WC()->cart->get_cart_contents_count().";\n";
+?>
+
+
+
+function doit()
+{
+  location.href="/wp/cart/?add-to-cart=44,59";
+}
+
+
+var x = document.getElementsByClassName("entry-content");
+var i;
+var bFound=false;
+for (i = 0; i < x.length; i++) 
+{
+  t=x[i];
+  if (t.innerHTML.indexOf("We sell Bath")>0)
+  {
+    bFound=true;
+    break;
+  }
+}
+if (bFound)
+{
+  //alert(t.innerHTML);
+  x="<font color=red>This page is experimental.</font>  <a href='https://thegr5store.com/wp/?product_cat=bath'>Click here to shop.</a>";
+  
+  x+="<p><input type='button' value='add multiple items to cart' onclick='doit()'>";
+  x+="<p>pretty if price: "+gr_pretty_price['if']+"<p>";
+  x+= "cart items: "+cart_items+"<p>";
+  x+= gr_img_cube;
+  t.innerHTML += x;
+}
+</script>
+
+
+<?php
+    
+
+
+}
+add_action('wp_footer', 'gr5_hook_javascript_footer');
+
+?>
---
+++ grstuff.js
@@ -0,0 +1,388 @@
+
+function doit()
+{
+  location.href="/wp/cart/?add-to-cart=44,59";
+}
+
+var fratios = [];
+var fratios_subset=[];
+
+class laser_lens
+{
+  constructor(_laser_type, _lens_fl, _minfr, _maxfr, _notes="")
+  {
+    this.laser_type = _laser_type;
+    this.lens_fl    = _lens_fl;
+    this.minfr      = _minfr;
+    this.maxfr      = _maxfr;
+    this.notes      = _notes;
+  }
+  
+  reset()
+  {
+    this.mirrors="";
+    this.qtyMirrorsNeed=0;
+    this.minMirror=1000; // fratio
+  }
+
+  addMirror(f)
+  {
+    ll.mirrors+=f+" ";
+    ll.qtyMirrorsNeed++;
+    if (f < this.minMirror) 
+      this.minMirror=f;
+  }  
+
+  
+  inrange(fr)
+  {
+    return (fr >= this.minfr && fr <= this.maxfr);
+  }
+  
+  laserPretty()
+  {
+    if (this.laser_type == "reg") return "Regular";
+    return this.laser_type;
+  }
+}
+
+function willLaserHandle(laser_type)
+{
+    // can we handle all focal ratios with this laser?
+    for (i=0; i < fratios_subset.length; i++)
+    {
+      fratio = fratios_subset[i];
+      for (j=0; j < laser_lens_combos.length; j++)
+      {
+        ll = laser_lens_combos[j];
+        if (ll.laser_type == laser_type && ll.inrange(fratio))
+          break; // handled
+      }
+      if (j==laser_lens_combos.length)return false;
+    }
+    return true;
+}
+
+function ll_resetAll()
+{
+  for (j=0; j < laser_lens_combos.length; j++)
+  {
+    laser_lens_combos[j].reset();
+  }
+}
+
+
+function selectLenses(laser_type_in)
+{
+    // assumes laser_lens_combos is sorted 
+    // 
+    for (i=0; i < fratios_subset.length; i++)
+    {
+      fratio = fratios_subset[i];
+      for (j = laser_lens_combos.length-1; j>=0; j--)
+      {
+        ll = laser_lens_combos[j];
+        if ( (ll.laser_type == laser_type_in || laser_type_in=="") && ll.inrange(fratio))
+        {
+          ll.addMirror(fratio);
+          break; // check next mirror
+        }
+      }
+    }
+}
+
+function maxMirrorForF(f)
+{
+  // convert f to a mirror diameter based on assuming Z8 term of 10 waves is maximum we can do
+  var diam = 10*8*.2496*f*f*f;
+  var digits = (diam<254) ? 1 : 0;
+  return (diam/25.4).toFixed(digits)+" in ("+diam.toFixed(0)+" mm)";
+}
+
+function display_chosen(div_id)
+{
+  var str="<table><tr><th>Laser</th><th>Lens</th><th>Mirror F/#</th><th>Max Mirror Diameter</th></tr>\n";
+  // reg
+  for (j=0; j < laser_lens_combos.length; j++)
+  {
+    ll = laser_lens_combos[j];
+    if (ll.laser_type !="reg")continue;
+    if (ll.qtyMirrorsNeed > 0)
+    {
+      str+="<tr><td>"+ll.laserPretty()+"</td><td>"+ll.lens_fl+"mm "+ll.notes+"</td><td>"+ll.mirrors+"</td>";
+      str+="<td>"+maxMirrorForF(ll.minMirror)+"</td></tr>\n";
+    }
+  }
+  // glass
+  for (j=0; j < laser_lens_combos.length; j++)
+  {
+    ll = laser_lens_combos[j];
+    if (ll.laser_type !="glass")continue;
+    if (ll.qtyMirrorsNeed > 0)
+    {
+      str+="<tr><td>"+ll.laserPretty()+"</td><td>"+ll.lens_fl+"mm "+ll.notes+"</td><td>"+ll.mirrors+"</td>";
+      str+="<td>"+maxMirrorForF(ll.minMirror)+"</td></tr>\n";
+    }
+  }
+  str+="</table>";
+  var t = document.getElementById(div_id);
+  t.innerHTML = str;
+}
+
+
+function gr_addF()
+{
+  var f = document.getElementById('gr_fnum').value;
+  gr_add_f(f);
+}
+
+function gr_add_f(f)
+{
+    f = parseFloat(f);
+    if (isNaN(f))
+      return; // not a number
+    if (f <= 0)
+      return;
+
+    var i;
+    for (i=0; i<fratios.length; ++i)
+    {
+      if (fratios[i] == f)
+        return; // already have this one
+    }
+    fratios.push(f);
+    fratios.sort(function(a, b) {return a-b;});
+    
+    gr_update_fs();
+}
+
+function save_cookie(name, value)
+{
+  var expires = (new Date(Date.now()+ 86400*365)).toUTCString(); // one year
+  document.cookie=name+"="+value+";expires="+expires+";path=/;";
+}
+
+const getCookieValue = (name) => (
+  document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+)
+
+function gr_update_fs()
+{
+  t = document.getElementById('gr_spn_sofar');
+  if (!t)
+    return;
+  if (fratios.length==0)
+  {
+    t.innerHTML="";
+    document.getElementById('gr_cont1').disabled="disabled";
+    save_cookie("grf",""); // delete cookie contents
+    choose_optics();
+    return;
+  }
+  
+  
+  var i;
+  var s="";
+  var c="";
+  for (i=0; i<fratios.length; ++i)
+  {
+     s+="F/"+fratios[i]+"&nbsp;&nbsp;";
+     c+=fratios[i]+",";
+  }
+  t.innerHTML="So far: "+s+" <font size=-1><a href='javascript:gr_clear_f();'>clear</a></font><br>";
+  save_cookie("grf",c);
+  document.getElementById('gr_cont1').disabled="";
+  choose_optics();
+}
+
+function gr_clear_f()
+{
+  fratios=[];
+  gr_update_fs();
+}
+
+function gr_go()
+{
+  var x = document.getElementsByClassName("entry-content");
+  var i;
+  var bFound=false;
+  for (i = 0; i < x.length; i++) 
+  {
+    t=x[i];
+    if (t.innerHTML.indexOf("We sell Bath")>0)
+    {
+      bFound=true;
+      break;
+    }
+  }
+  if (!bFound) return;
+  //alert(t.innerHTML);
+  x="<font color=red>This page is experimental.</font>  <a href='https://thegr5store.com/wp/?product_cat=bath'>Click here to shop.</a><p>";
+
+  x+="Use this page to help you pick out an inexpensive Bath Interferometer. ";
+  x+="<br>";
+  x+="Step 1 - choose laser and diverger<br>\n";
+  x+="Please enter F/#'s of a mirror you want to test. Then press enter or click add button until you have listed them all.<br>\n";
+  x+="example   4    for f/4 <br>\n";
+  x+="F/#&nbsp;";
+  x+="<input type='text' id='gr_fnum' value=6 size=2";
+  x+=" onkeydown = 'if (event.keyCode==13)gr_addF()' >";
+  x+="<input type='button' value='add' onclick='gr_addF()'><br>\n";
+  x+="<span id='gr_spn_sofar'></span><br>\n";
+  x+="<div id='idOptics1'></div>\n";
+  x+="<div id='idOptics2'></div>\n";
+  x+="<div id='idOptics3'></div>\n";
+  x+="<div id='idOptics4'></div>\n";
+  
+  x+="<input type='button' value='Continue with this option (still under construction)' id='gr_cont1'><br>\n";
+  
+/*
+
+  x+="<p><input type='button' value='add multiple items to cart' onclick='doit()'>";
+  x+="<p>pretty if price: "+gr_pretty_price['if']+"<p>";
+  x+= "cart items: "+cart_items+"<p>";
+  x+= gr_img_cube;
+  
+  */
+  
+  t.innerHTML += x;
+  gr_load_cookies();
+  gr_update_fs();
+  //document.cookie="a=hay";
+  //document.cookie="b=bee";
+ 
+  choose_optics();
+  
+}
+
+var laser_lens_combos = [];
+// must be sorted by minimum F/# column
+laser_lens_combos.push( new laser_lens("glass", 7.65, 1.8, 4.4) );
+laser_lens_combos.push( new laser_lens("glass", 9,    2.6, 5.2) );
+laser_lens_combos.push( new laser_lens("reg",   7.65, 3.3, 6.6) );
+laser_lens_combos.push( new laser_lens("reg",   9,    3.6, 7.2) );
+laser_lens_combos.push( new laser_lens("glass", 13,   3.7, 7.4) );
+laser_lens_combos.push( new laser_lens("reg",   13,   4.7, 10) );
+
+// edmunds lenses
+laser_lens_combos.push( new laser_lens("glass", 18,   6.1, 10, "(Edmund Optics 32-966)") ); // f/5 minimum.  6.1 chosen to give priority to 13mm lens
+laser_lens_combos.push( new laser_lens("reg",   18,   8, 15,   "(Edmund Optics 32-966)") );
+laser_lens_combos.push( new laser_lens("glass", 30,   8.5, 15, "(Edmund Optics 45-133)") );
+laser_lens_combos.push( new laser_lens("glass", 40,   10, 20,  "(Edmund Optics 63-540)") );
+laser_lens_combos.push( new laser_lens("reg",   30,   13, 25,  "(Edmund Optics 45-133)") );
+laser_lens_combos.push( new laser_lens("reg",   40,   17, 34,  "(Edmund Optics 63-540)") );
+
+
+
+
+
+var result="";
+
+
+function gr_load_cookies()
+{
+  //alert("v2");
+  var c = getCookieValue("grf");
+  if (!c) return;
+  var ary = c.split(",");
+  for(var i=0; i<ary.length; i++)
+  {
+    gr_add_f(ary[i]);
+  }
+}
+
+function choose_optics()
+{
+    ll_resetAll();
+    
+    fratios_subset = fratios.slice(); // make a copy
+
+    
+    var t = document.getElementById("idOptics1");
+    t.innerHTML="";
+    document.getElementById("idOptics2").innerHTML="";
+    document.getElementById("idOptics3").innerHTML="";
+    document.getElementById("idOptics4").innerHTML="";
+
+    if (fratios_subset.length==0)return;
+    
+    var result="";
+
+    // check for mirrors not in the ranges
+    for (i=0; i < fratios_subset.length; i++)
+    {
+      fratio = fratios_subset[i];
+      if (fratio < laser_lens_combos[0].minfr)
+      {
+        result+="F/"+fratio+" mirror (less than F/"+laser_lens_combos[0].minfr+
+                ") is pretty extreme - please contact us about testing this mirror<br>";
+        fratios_subset.splice(i,1); // remove this one from the array
+        i--;
+      }
+      else if (fratio > laser_lens_combos[laser_lens_combos.length-1].maxfr)
+      {
+        result+="F/"+fratio+" mirror (greater than F/"+laser_lens_combos[laser_lens_combos.length-1].maxfr+
+                ") will need a longer focal length lens than I normally sell but these are available - please contact us about testing this mirror<br>";
+        fratios_subset.splice(i,1); // remove this one from the array
+        i--;
+      }
+    }
+
+
+
+    if (willLaserHandle("reg"))
+    {
+      selectLenses("reg");
+      display_chosen("idOptics2");
+      if (willLaserHandle("glass"))
+      {
+        ll_resetAll();
+        selectLenses("glass");
+        document.getElementById("idOptics3").innerHTML = 
+          "<p>Or alternatively get the glass laser and these lenses:";
+        display_chosen("idOptics4");
+      }
+    }
+    else
+    {
+      selectLenses("glass");
+      display_chosen("idOptics2");
+    }
+
+    /*
+    else if (willLaserHandle("glass"))
+    {
+      selectLenses("glass");
+      display_chosen();
+    } else
+    {
+      selectLenses("");
+      display_chosen();
+    }
+    */
+
+
+
+    t.innerHTML = result;
+
+    /*
+                 min max
+    glass  7.65  2.2 4.4
+    glass  9     2.6 5.2
+    reg    7.65  3.3 6.6
+    reg    9     3.6 7.2
+    glass 13     3.7 7.4
+    reg   13     4.7 10
+
+    1) Try to resolve all with reg laser
+    if not
+    2) Try to resolve all with just glass laser
+
+    If we are doing all with reg laser:
+    1) choose largest FL lens that works with each mirror
+
+    */
+
+}
+
+
